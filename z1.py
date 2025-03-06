7aff9ee2b2b27f7b986564b82a515543ce0235ba
Lovro Lešić <109683016+Lekaa29@users.noreply.github.com>
2025-03-06 13:14:25 +0100

    Add files via upload

---
+++ z1.py
@@ -0,0 +1,17 @@
+
+"""
+Zadatak 1.4.1 Napišite program koji od korisnika zahtijeva unos radnih sati te koliko je placen ´
+po radnom satu. Koristite ugradenu Python metodu ¯ input(). Nakon toga izracunajte koliko ˇ
+je korisnik zaradio i ispišite na ekran. Na kraju prepravite rješenje na nacin da ukupni iznos ˇ
+izracunavate u zasebnoj funkciji naziva ˇ total_euro.
+Primjer:
+Radni sati: 35 h
+eura/h: 8.5
+Ukupno: 297.5 eura
+"""
+radniSati = int(input("Radni sati: "))
+satnica = float(input("eura/h: "))
+
+
+print(f"Ukupno: {radniSati * satnica}")
+
